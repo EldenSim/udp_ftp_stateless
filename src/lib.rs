4d2aaafd319056f61a99726f1dd0caee0985d14d
EldenSim <elden.str@gmail.com>
2024-01-09 14:37:25 +0800

    Added sending logic

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 pub type Error = Box<dyn std::error::Error>; // For early dev.
 
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 // endregion: --- Modules
@@ -11,16 +12,17 @@ use uuid::Uuid;
 /*
 Packet structure:
     Preamble (Limit to 64 bytes):
-        - 0xAA | Filname | filesize | chunk_id | number_of_chunks_expected | segment_id | number of segments_expected |
+        - 0xAA | filename | filesize | chunk_id | number_of_chunks_expected | segment_id | number of segments_expected |
     Data (Limit to MTU - IP/UDP header (28 bytes) - Preamble) -- target (9000 - 30 - 64 = 8506 ~ 8500):
         - 0xAA | data | 0xAA
 */
 
 // -- Template for preamble info (64 bytes)
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Preamble {
     // -- Max filename size limit to 16 bytes for now
     // -- TODO: Change to uuid format
-    pub filename: Uuid,
+    pub filename: String,
     // -- 8 bytes
     pub filesize: u64,
     // -- 8 bytes
@@ -35,10 +37,17 @@ pub struct Preamble {
 }
 
 // -- Template for Packet (Maximum 9000)
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Packet {
     pub pre_padding: u8,
     pub preamble: Preamble,
     pub mid_padding: u8,
     pub data: Vec<u8>,
     pub post_padding: u8,
+}
+
+// -- Encoding Configuration Variables
+pub struct EncoderConfig {
+    pub max_source_symbol_size: usize,
+    pub number_of_repair_symbols: usize,
 }
--- src/send/mod.rs
+++ src/send/mod.rs
@@ -1,12 +1,114 @@
 // region:    --- Modules
 
 use std::net::UdpSocket;
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::{env, fs};
 
 use udp_ftp_stateless::Result;
-use udp_ftp_stateless::{Packet, Preamble};
+use udp_ftp_stateless::{EncoderConfig, Packet, Preamble};
+
+use uuid::{uuid, Uuid};
 
 // endregion: --- Modules
 
 pub fn main(udp_service: &UdpSocket) -> Result<()> {
+    // region:    --- Loop through sending folder
+    let SENDING_DIRECTORY =
+        env::var("SENDING_DIRECTORY").expect("SENDING_DIRECTORY env var not set");
+    let NUMBER_OF_REPAIR_SYMBOLS = env::var("NUMBER_OF_REPAIR_SYMBOLS")
+        .expect("NUMBER_OF_REPAIR_SYMBOLS env var not set")
+        .parse::<usize>()?;
+    let MAX_SOURCE_SYMBOL_SIZE = env::var("MAX_SOURCE_SYMBOL_SIZE")
+        .expect("MAX_SOURCE_SYMBOL_SIZE env var not set")
+        .parse::<usize>()?;
+
+    let chunksize = calculate_chunksize_and_data_size(MAX_SOURCE_SYMBOL_SIZE)?;
+
+    let encoder_config = EncoderConfig {
+        max_source_symbol_size: MAX_SOURCE_SYMBOL_SIZE,
+        number_of_repair_symbols: NUMBER_OF_REPAIR_SYMBOLS,
+    };
+    for entry in fs::read_dir(SENDING_DIRECTORY)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            unimplemented!()
+        } else {
+            send_file_with_raptor(&path, udp_service, chunksize, &encoder_config)?;
+        }
+    }
+
+    // endregion: --- Loop through sending folder
     Ok(())
+}
+
+fn send_file_with_raptor(
+    path: &PathBuf,
+    udp_service: &UdpSocket,
+    chunksize: usize,
+    encoder_config: &EncoderConfig,
+) -> Result<()> {
+    // -- Obtain Filename and Filesize for preamble
+    let filename = path.file_name().unwrap().to_str().unwrap().to_string();
+    let file = fs::read(path)?;
+    let filesize = file.len();
+    // -- Obtain chunksize and max data size variable from MTU and max chunksize limits
+
+    let chunks: Vec<_> = file.chunks(chunksize).collect();
+    let total_number_of_chunks = chunks.len();
+    for (chunk_id, chunk) in chunks.iter().enumerate() {
+        // println!("->> Chunk id: {}", chunk_id);
+
+        let mut encoder =
+            raptor_code::SourceBlockEncoder::new(chunk, encoder_config.max_source_symbol_size);
+        let total_num_of_symbols =
+            encoder.nb_source_symbols() as usize + encoder_config.number_of_repair_symbols;
+
+        for segment_id in 0..total_num_of_symbols {
+            // println!("\t->> Segmnet id: {}", segment_id);
+            let encoding_symbol = encoder.fountain(segment_id as u32);
+
+            let preamble = Preamble {
+                filename: filename.clone(),
+                filesize: filesize as u64,
+                chunk_id: chunk_id as u64,
+                number_of_chunks_expected: total_number_of_chunks as u64,
+                segment_id: segment_id as u64,
+                number_of_segments_expected: total_num_of_symbols as u64,
+            };
+            // println!("->> Preamble: {:#?}", preamble);
+            let packet = Packet {
+                pre_padding: 170,
+                preamble,
+                mid_padding: 170,
+                data: encoding_symbol,
+                post_padding: 170,
+            };
+            // println!("->> Packet: {:?}", packet);
+            let packet_bytes = bincode::serialize(&packet)?;
+            udp_service.send(&packet_bytes).expect("Send packet error");
+        }
+        break;
+    }
+
+    Ok(())
+}
+
+fn calculate_chunksize_and_data_size(max_source_symbol_size: usize) -> Result<usize> {
+    // -- MTU limit will determine the size of packet
+    let mtu_limit = env::var("MTU")
+        .expect("MTU env var not set")
+        .parse::<usize>()?;
+
+    // -- Max data size is calculate by deducting preamble (64 bytes), IP/ UDP Headers (28 bytes) and buffer (10 bytes).
+    let max_data_size = mtu_limit - 64 - 28 - 10;
+    const MAX_CHUNKSIZE: usize = 500_000;
+    let chunksize = if (mtu_limit * max_source_symbol_size) > MAX_CHUNKSIZE {
+        MAX_CHUNKSIZE
+    } else {
+        mtu_limit * max_source_symbol_size
+    };
+    // -- Note usize conversion may have error in future
+    Ok(chunksize)
 }
