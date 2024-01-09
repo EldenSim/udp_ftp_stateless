bab472702db23dedb70e4f9dda54d90e0a34ec34
EldenSim <elden.str@gmail.com>
2024-01-09 13:12:39 +0800

    added print for udp init and connection

--- src/udp/mod.rs
+++ src/udp/mod.rs
@@ -10,7 +10,8 @@ pub fn init_udp_service(LOCAL_ADDRESS: &str, port: &str) -> Result<UdpSocket> {
     // -- Initialise UDP port
     // let port = 8000;
     let udp_addr = format!("{}:{}", LOCAL_ADDRESS, port);
-    let udp_service = UdpSocket::bind(udp_addr)?;
+    let udp_service = UdpSocket::bind(&udp_addr)?;
+    println!("UDP Socket initialised at {}", udp_addr);
     Ok(udp_service)
 }
 
@@ -22,6 +23,7 @@ pub fn connect_to_foreign_addr(
     // -- Connect to sending address
     // let foreign_port = 8001;
     let foreign_addr = format!("{}:{}", FOREIGN_ADDRESS, foreign_port);
-    udp_service.connect(foreign_addr)?;
+    udp_service.connect(&foreign_addr)?;
+    println!("UDP Socket connected to {}", foreign_addr);
     Ok(())
 }
