9a832538b4edf60e612c94a62953a42e9c9e5a42
EldenSim <elden.str@gmail.com>
2024-01-10 11:46:00 +0800

    Added segment storing

--- src/recv/mod.rs
+++ src/recv/mod.rs
@@ -2,11 +2,13 @@
 
 use std::collections::HashMap;
 use std::env;
+use std::mem::size_of_val;
 use std::net::UdpSocket;
 
 use udp_ftp_stateless::Result;
 use udp_ftp_stateless::{Packet, Preamble};
 
+use async_std::sync::{Arc, Mutex};
 use async_std::{fs, task};
 
 // endregion: --- Modules
@@ -35,74 +37,46 @@ pub fn main(udp_service: &UdpSocket) -> Result<()> {
     //     Box::new(HashMap::new());
     // let mut segment_hashmap: Box<HashMap<String, Vec<u8>>> = Box::new(HashMap::new());
     let mut file_chunk_segment_counter: HashMap<String, Vec<Vec<String>>> = HashMap::new();
-    let mut segment_hashmap: HashMap<String, Vec<u8>> = HashMap::new();
+    let mut segment_hashmap: Arc<Mutex<Box<HashMap<String, Vec<u8>>>>> =
+        Arc::new(Mutex::new(Box::new(HashMap::new())));
+    let mut temp_storage = Box::new(Vec::new());
     loop {
-        let max_packet_size = env::var("MTU")
-            .expect("MTU env var not set")
-            .parse::<usize>()?;
-        let mut buffer = vec![0; max_packet_size];
+        let mut buffer = vec![0; MTU];
         match udp_service.recv_from(&mut buffer) {
             Ok((bytes, _)) => {
                 let recv_data = &buffer[..bytes];
-
-                // region:    --- Parsing data
-
-                let packet: Packet = bincode::deserialize(recv_data)?;
-                let preamble = packet.preamble;
-                let filename = preamble.filename;
-                let chunk_id = preamble.chunk_id as usize;
-                let number_of_chunks_expected = preamble.number_of_chunks_expected as usize;
-                let segment_id = preamble.segment_id as usize;
-                let number_of_segments_expected = preamble.number_of_segments_expected as usize;
-                // -- Check if filename already exist in Hashmap
-                if !file_chunk_segment_counter.contains_key(&filename) {
-                    let segment_vec = vec!["".to_string()];
-                    let chunk_vec = vec![segment_vec; number_of_chunks_expected];
-                    file_chunk_segment_counter.insert(filename.clone(), chunk_vec);
-                }
-                let file_chunk_segment_name =
-                    format!("{}_c_{}_s_{}", &filename, chunk_id, segment_id);
-                println!("->> F C S name: {}", file_chunk_segment_name);
-                file_chunk_segment_counter.get_mut(&filename).unwrap()[chunk_id]
-                    .insert(segment_id, file_chunk_segment_name.clone());
-
-                segment_hashmap.insert(file_chunk_segment_name, packet.data);
-                // endregion: --- Parsing data
-
-                // region:    --- Processing Segment
-
-                // -- Check if received enough segments
-                // println!("->> Segment len: {:?}", segment_hashmap);
-                if file_chunk_segment_counter.get(&filename).unwrap()[chunk_id].len()
-                    > (number_of_segments_expected - NUMBER_OF_REPAIR_SYMBOLS)
-                {
-                    let segments_name_to_decode =
-                        file_chunk_segment_counter.get(&filename).unwrap()[chunk_id].clone();
-                    let mut segments_to_decode = Vec::new();
-                    for segment in segments_name_to_decode.iter() {
-                        if let Some(bytes) = segment_hashmap.get(segment) {
-                            segments_to_decode.push(bytes.clone())
-                        }
-                        segment_hashmap.remove(segment);
-                    }
-                    println!("->> Number of segments: {}", segments_to_decode.len());
-                    task::spawn(async move {
-                        decode_segments(
-                            segments_to_decode,
-                            segments_name_to_decode,
-                            MAX_SOURCE_SYMBOL_SIZE,
-                            chunksize,
-                            filename.clone(),
-                            chunk_id,
-                        )
-                        .await
-                    });
-                }
-
-                // endregion: --- Processing Segment
+                temp_storage.push(recv_data.to_vec());
             }
             Err(e) => return Err(e.into()),
         }
+        // -- Parse packet and name them
+        let move_temp_storage: Vec<_> = temp_storage.drain(..temp_storage.len()).collect();
+        let move_segment_template = Arc::clone(&segment_hashmap);
+        task::spawn(async move {
+            for packet in move_temp_storage.iter() {
+                let packet: Packet =
+                    bincode::deserialize(&packet).expect("Unable to deserialise packet.");
+                let preamble = packet.preamble;
+                let filename = preamble.filename;
+                let chunk_id = preamble.chunk_id;
+                let segment_id = preamble.segment_id;
+                let file_chunk_segment_name =
+                    format!("{}_c_{}_s_{}", &filename, chunk_id, segment_id);
+                // segment_hashmap.insert(file_chunk_segment_name, packet.data);
+                move_segment_template
+                    .lock()
+                    .await
+                    .insert(file_chunk_segment_name, packet.data);
+            }
+            // println!(
+            //     "->> segment hashmap: {:?}",
+            //     move_segment_template.lock().await.len()
+            // );
+            // println!(
+            //     "->> Mem segment hashmap {}",
+            //     size_of_val(&**move_segment_template.lock().await)
+            // );
+        });
     }
 
     Ok(())
@@ -134,4 +108,60 @@ async fn decode_segments(
     fs::write(chunk_file_path, reconstructed_chunk)
         .await
         .unwrap()
+}
+
+mod temp {
+    // region:    --- Parsing data
+
+    // let packet: Packet = bincode::deserialize(recv_data)?;
+    // let preamble = packet.preamble;
+    // let filename = preamble.filename;
+    // let chunk_id = preamble.chunk_id as usize;
+    // let number_of_chunks_expected = preamble.number_of_chunks_expected as usize;
+    // let segment_id = preamble.segment_id as usize;
+    // let number_of_segments_expected = preamble.number_of_segments_expected as usize;
+    // // -- Check if filename already exist in Hashmap
+    // if !file_chunk_segment_counter.contains_key(&filename) {
+    //     let segment_vec = vec!["".to_string()];
+    //     let chunk_vec = vec![segment_vec; number_of_chunks_expected];
+    //     file_chunk_segment_counter.insert(filename.clone(), chunk_vec);
+    // }
+    // let file_chunk_segment_name =
+    //     format!("{}_c_{}_s_{}", &filename, chunk_id, segment_id);
+    // println!("->> F C S name: {}", file_chunk_segment_name);
+    // file_chunk_segment_counter.get_mut(&filename).unwrap()[chunk_id]
+    //     .insert(segment_id, file_chunk_segment_name.clone());
+
+    // segment_hashmap.insert(file_chunk_segment_name, packet.data);
+    // // endregion: --- Parsing data
+
+    // // region:    --- Processing Segment
+
+    // // -- Check if received enough segments
+    // // println!("->> Segment len: {:?}", segment_hashmap);
+    // if file_chunk_segment_counter.get(&filename).unwrap()[chunk_id].len()
+    //     > (number_of_segments_expected - NUMBER_OF_REPAIR_SYMBOLS)
+    // {
+    //     let segments_name_to_decode =
+    //         file_chunk_segment_counter.get(&filename).unwrap()[chunk_id].clone();
+    //     let mut segments_to_decode = Vec::new();
+    //     for segment in segments_name_to_decode.iter() {
+    //         if let Some(bytes) = segment_hashmap.get(segment) {
+    //             segments_to_decode.push(bytes.clone())
+    //         }
+    //         segment_hashmap.remove(segment);
+    //     }
+    //     println!("->> Number of segments: {}", segments_to_decode.len());
+    //     task::spawn(async move {
+    //         decode_segments(
+    //             segments_to_decode,
+    //             segments_name_to_decode,
+    //             MAX_SOURCE_SYMBOL_SIZE,
+    //             chunksize,
+    //             filename.clone(),
+    //             chunk_id,
+    //         )
+    //         .await
+    //     });
+    // }
 }
